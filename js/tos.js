03345137f326d38417343dab914d0c83afb09307
rippergt <75691477+rippergt@users.noreply.github.com>
2020-12-08 21:41:23 +0400

    Add files via upload

---
+++ js/tos.js
@@ -0,0 +1,26 @@
+function enableButton()
+{
+	if(document.getElementById("tosbox").checked == true)
+	{
+		document.getElementById("buybutton").disabled = false;
+		document.getElementById("buybutton").className = "enabledbutton";
+	}
+	if(document.getElementById("tosbox").checked == false)
+	{
+		document.getElementById("buybutton").disabled = true;
+		document.getElementById("buybutton").className = "disabledbutton";
+	}
+}
+
+function buy() {
+window.open("discord.gg/MPSFS8CcYm",
+				   'targetWindow',
+                                   `toolbar=no,
+                                    location=no,
+                                    status=no,
+                                    menubar=no,
+                                    scrollbars=yes,
+                                    resizable=yes,
+                                    width=400,
+                                    height=600`);
+}
